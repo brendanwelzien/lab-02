c7a33ecbb5c919ae1640fb37c492d503a437873a
Brendan Welzien <brendanwelzien@gmail.com>
2020-10-06 15:22:15 -0700

    feature 1

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+
+
+// template for holding content
+function Animal(animal) {
+    this.image_url = animal.image_url;
+    this.title = animal.title;
+    this.description = animal.description;
+    this.keyword = animal.keyword;
+    this.horns = animal.horns;
+
+}
+
+// create a copy of the animal
+
+Animal.prototype.render = function() {
+    let $animalClone = $('.photo').clone();
+   
+
+// apply information to elements
+    $animalClone.find('h2').text(this.title);
+    $animalClone.find('img').attr('src', this.image_url);
+    $animalClone.find('p').text(this.description);
+    $animalClone.removeClass('photo');
+    $animalClone.attr('class', this.title);
+    $('main').append($animalClone);
+    console.log($animalClone);
+}
+
+// grab/update content of dog info from json package
+
+Animal.readJson = () => {
+    const ajaxFrame = {
+        method: 'get',
+        dataType: 'json'
+    }
+    $.ajax('page-1.json', ajaxFrame)
+        .then(info => {
+            info.forEach(item => {
+                let animal = new Animal(item);
+                console.log('dog obj', animal);
+                animal.render();
+            })
+        })
+}
+$(() => Animal.readJson());
